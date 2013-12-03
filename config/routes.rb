9b496be6e5e648ab263165506bc0edbad83797c4
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-02 22:49:47 -0700

    Added home route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,7 @@ ProjectSabertooth::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :login
+  resources :home
 
   # Example resource route with options:
   #   resources :products do
