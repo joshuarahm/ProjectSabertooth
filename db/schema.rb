7a0c5155f9636af8dedb279944d8463997e63985
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-03 22:06:59 -0700

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131118000353) do
+ActiveRecord::Schema.define(version: 20131204044408) do
+
+  create_table "user_sessions", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
+    t.string   "displayName"
+    t.string   "emailAddress"
+    t.integer  "plan"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
