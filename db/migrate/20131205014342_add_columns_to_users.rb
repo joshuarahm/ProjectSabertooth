11372f2656b195e220585b867dd8244c20638203
Joshua Rahm <joshua.rahm@colorado.edu>
2013-12-04 18:52:20 -0700

    added migration files to add password, salt and session token

---
+++ db/migrate/20131205014342_add_columns_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :password_hash, :string
+    add_column :users, :password_salt, :string
+    add_column :users, :session_token, :string
+  end
+end
