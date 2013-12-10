0982cc9ddd71631b3cfa5ed8053a7eac4d14b465
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-10 14:33:19 -0700

    Moved login helper functions into login_helpers.rb.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,4 +1,5 @@
 class LoginController < ApplicationController
+	include LoginHelper
 	#The default login page
 	def index
         @user = User
@@ -44,38 +45,4 @@ class LoginController < ApplicationController
         newUser.save()
 	end
 
-    def newSalt()
-        salt = ""
-        salt << newNonce(8)
-        return Base64.encode64(salt)
-    end
-
-    def newNonce(len)
-        nonce = ""
-        for i in 0..len
-            nonce << rand(256)
-        end
-        return nonce
-    end
-
-    #Takes a plaintext password and a Base64 encoded salt and computes the hash
-    def hashPassword(password, salt)
-        pwHash = Digest::SHA2.new(256) 
-        pwHash << password << Base64.decode64(salt)
-        return pwHash.to_s
-    end
-
-    def getUser(emailAddress, password)
-        possibleUser = User.where(:emailAddress=>emailAddress).first
-        if possibleUser == nil
-            print("User not found!")
-            return nil
-        end
-        print(possibleUser)
-        paramPass = hashPassword(password, possibleUser[:password_salt])
-        if paramPass == possibleUser[:password_hash]
-            return possibleUser
-        end
-        return nil
-    end
 end
--- app/helpers/login_helper.rb
+++ app/helpers/login_helper.rb
@@ -1,2 +1,36 @@
 module LoginHelper
+    def newSalt()
+        salt = ""
+        salt << newNonce(8)
+        return Base64.encode64(salt)
+    end
+
+    def newNonce(len)
+        nonce = ""
+        for i in 0..len
+            nonce << rand(256)
+        end
+        return nonce
+    end
+
+    #Takes a plaintext password and a Base64 encoded salt and computes the hash
+    def hashPassword(password, salt)
+        pwHash = Digest::SHA2.new(256) 
+        pwHash << password << Base64.decode64(salt)
+        return pwHash.to_s
+    end
+
+    def getUser(emailAddress, password)
+        possibleUser = User.where(:emailAddress=>emailAddress).first
+        if possibleUser == nil
+            print("User not found!")
+            return nil
+        end
+        print(possibleUser)
+        paramPass = hashPassword(password, possibleUser[:password_salt])
+        if paramPass == possibleUser[:password_hash]
+            return possibleUser
+        end
+        return nil
+    end
 end
