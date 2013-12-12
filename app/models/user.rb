203b8ce86208f9bd9b3e36accc31cfd62bfb432f
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-12 12:26:02 -0700

    Rspec tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ActiveRecord::Base
-	validates :email_address, uniqueness: true
+	validates :email_address, uniqueness: true, presence: true
+	validates :password_hash, presence: true
+	validates :password_salt, presence: true
+#	validate :has_valid_plan
 
+
+#	def has_valid_plan
+#		if plan != nil and (plan < 0 or plan > 3)
+#			errors.add( :plan, "should be between 0 and 3, inclusive. (There are only 4 plans")
+#		end
+#	end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,39 @@
 require 'spec_helper'
+require 'base64'
+require 'digest'
 #require 'User'
 
 describe User do
 	it "should require an email address" do
-		testUser = User.new
-		testUser.valid?().should == false
+		subject.valid?().should == false
+		subject[:email_address] = "test@test.com"
+		subject.valid?().should == true
 	end
-	it "should require a password at least 5 characters long"
+	it "should require a password (and salt)" do
+		subject[:email_address] = "test@test.com"
+		subject.valid?().should == false
+		subject[:password_hash] = "abcdefghijklmnop"
+		subject[:password_salt] = "abcd"
+		subject.valid?().should == true
+	end
 	it "should require a valid plan type"
-	it "should accept a display name"
-	it "should accept a plan type"
-	it "should accept a password hash and salt"
-	it "should accept a session token"
+	it "should accept a display name" do
+		subject[:display_name] = "bob"
+		subject[:display_name].should == "bob"
+	end
+	it "should accept a session token" do
+		subject[:session_token] = "a91dvjsndkfoasdifj"
+		subject[:session_token].should == "a91dvjsndkfoasdifj"
+	end
 end
 
 describe "A User's session" do
-	it "should be properly stored in the User model"
+	it "should be properly stored in the User model" do
+		subject[:email_address] = "test@test.com"
+		tmp = Digest::SHA2(256)
+		tmp << "testtoken"
+		subject[:session_token] = tmp.to_s
+		subject[:email_address].should == "test@test.com"
+		subject[:session_token].should == tmp.to_s
 	it "should store a hashed token."
 end
