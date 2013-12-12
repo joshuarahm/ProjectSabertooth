92f2a6b3837b723a2b031e728dc0f4d01ce2b735
ZachAnders <Zacharia.Anders@gmail.com>
2013-12-12 14:27:11 -0700

    Text loading in document index.

--- app/controllers/document_controller.rb
+++ app/controllers/document_controller.rb
@@ -1,4 +1,14 @@
 class DocumentController < ApplicationController
 	def index
+		if @user_id != nil
+			@documents = Document.where(:owner_id=>@user_id)
+			if session[:current_doc] != nil
+				@current_doc = Document.find(session[:current_doc])
+			end
+		end
+	end
+	def show
+		session[:current_doc] = params[:id]
+		redirect_to document_index_path
 	end
 end
